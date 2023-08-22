ceef1ef3ad34440d49a3244eb691c9ccba865417
adiamandouros <adiamandouros@gmail.com>
2023-08-22 20:03:08 +0300

    first roster attempt

---
+++ yolk.js
@@ -0,0 +1,19 @@
+let finalKeys = []
+
+document.addEventListener('keydown', (event) => {
+    finalKeys.push(event.key);
+    console.log(finalKeys);
+    if(finalKeys.length>=4){
+        console.log(finalKeys);
+        if(finalKeys[0]==1 && finalKeys[1]==3 && finalKeys[2]==1 && finalKeys[3]==2){
+            let egg = document.getElementsByClassName('egg')[0];
+            egg.style.opacity = 0.5;
+        }
+        finalKeys = finalKeys.slice(1);
+    }
+  }, false);
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
