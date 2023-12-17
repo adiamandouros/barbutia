4ebf7fbe44cecf43c207ddafea1beb3af159d95e
adiamandouros <adiamandouros@gmail.com>
2023-12-17 22:09:41 +0200

    visual improvements in small screens

--- standings.js
+++ standings.js
@@ -7,7 +7,7 @@ fetch('https://yioka.eu/api/mparmpoutia.php?method=leagues&id=25')
 
     data.forEach(team => {
         const row = document.createElement('div');
-        row.classList.add('main-element', 'bg-dark', 'my-1', 'py-2', 'row', 'navbar-expand-lg', 'align-items-center');
+        row.classList.add('main-element', 'bg-dark', 'my-1', 'py-2', 'row', 'navbar-expand-lg', 'align-items-center', 'flex-nowrap');
         row.innerHTML='<div class="col-1">' + i + '</div>' +
                         '<div class="col-1 collapse navbar-collapse justify-content-end flex-grow-0 flex-shrink-1"><img src="https://storage.googleapis.com/' + team.imageUrl.replaceAll('"','') + '" alt="team logo" class="smallLogo"></div>' +
                         '<div class="col fw-bold">' + team.name + '</div>' +
