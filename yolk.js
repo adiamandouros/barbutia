1dcb5fda2c40c89a82767905c495ac1f50c7f937
adiamandouros <adiamandouros@gmail.com>
2023-12-18 02:20:22 +0200

    easter egg for mobile

--- yolk.js
+++ yolk.js
@@ -2,8 +2,6 @@ let finalKeys = []
 
 document.addEventListener('keydown', (event) => {
     finalKeys.push(event.key);
-    alert(event.keyCode);
-    console.log(finalKeys);
     if(finalKeys.length>=4){
         console.log(finalKeys);
         if(finalKeys[0]==1 && finalKeys[1]==3 && finalKeys[2]==1 && finalKeys[3]==2){
@@ -15,6 +13,16 @@ document.addEventListener('keydown', (event) => {
     }
   }, false);
 
+document.addEventListener('keyup', (event)=>{
+    const inputs = document.querySelectorAll('input');
+    inputs.foreach(element => {
+      if(element.value==1312) {
+        let egg = document.getElementsByClassName('egg')[0];
+        egg.style.visibility = "visible";
+        egg.style.opacity = 0.5;
+      }
+    })
+}, false);
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
