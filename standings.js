4cfb48bb9a20d408d46bb8655ab338f7bf774b85
adiamandouros <adiamandouros@gmail.com>
2023-12-20 01:40:21 +0200

    fixed standings result

--- standings.js
+++ standings.js
@@ -5,22 +5,11 @@ fetch('https://yioka.eu/api/mparmpoutia.php?method=leagues&id=25')
     console.log(data);
     let i=1;
 
-    data.forEach(team => {
-        // console.log(team);
-        // const row = document.createElement('div');
-        // row.classList.add('main-element', 'bg-dark', 'my-1', 'py-2', 'row', 'navbar-expand-lg', 'align-items-center', 'flex-nowrap');
-        // row.innerHTML='<div class="col-1">' + i + '</div>' +
-        //                 '<div class="col-1 collapse navbar-collapse justify-content-end flex-grow-0 flex-shrink-1"><img src="https://storage.googleapis.com/' + team.imageUrl.replaceAll('"','') + '" alt="team logo" class="smallLogo"></div>' +
-        //                 '<div class="col fw-bold">' + team.name + '</div>' +
-        //                 '<div class="col">' + team.standing + '</div>' +
-        //                 '<div class="col">' + team.gp + '</div>' +
-        //                 '<div class="col">' + team.won + '</div>' +
-        //                 '<div class="col">' + team.lost + '</div>' +
-        //                 '<div class="col">' + team.pointsPlus + '</div>' +
-        //                 '<div class="col">' + team.pointsMinus + '</div>';
-        // i++;
+    data.sort((a,b) => {
+        return b.standing - a.standing || b.pointsDiff-a.pointsDiff;
+    });
 
-        // document.querySelector('tbody').appendChild(row);
+    data.forEach(team => {
         const row = document.createElement('tr');
         // row.classList.add('navbar-expand-lg');
         const th = document.createElement('th');
@@ -63,4 +52,7 @@ fetch('https://yioka.eu/api/mparmpoutia.php?method=leagues&id=25')
         document.querySelector('tbody').appendChild(row);
         i++;
     })
+})
+.then(()=> {
+
 });
